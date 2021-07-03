17755c9f73e28c339819273de804a233c9ed9e6d
anookinov <anookinov@gmail.com>
2021-07-03 16:30:00 +0700

    add server and servers components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  // prettier-ignore
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `],
 })
-export class AppComponent {
-  name = 'Nookinov';
-}
+export class AppComponent {}
